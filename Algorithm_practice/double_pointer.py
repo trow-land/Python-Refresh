75a24ff1aa30a1602c07e8dfb67ca395346b256f
Tom Rowland <75323342+trow-land@users.noreply.github.com>
2023-11-01 19:58:47 +0000

    Update double_pointer.py

--- Algorithm_practice/double_pointer.py
+++ Algorithm_practice/double_pointer.py
@@ -1,4 +1,4 @@
-# Find the Pair that Sums to a Target Value using two pointers
+# Find the Pair that Sums to a Target Value: Given an array of integers arr and an integer target, find two numbers such that they add up to the target
 array = [1, 2, 3, 4, 6]
 targ = 6
 
@@ -38,7 +38,7 @@ def double_pointer(arr, target):
 
 print(double_pointer(array, targ))  # [1, 3]
 
-# Reverse a String using two pointers
+# Reverse a String: Given a string, write a function to reverse it using a two-pointer technique.
 
 sentence = "Hello, world!"
 
@@ -64,7 +64,7 @@ def reverse_str(string):
 
 print(reverse_str(sentence))  # !dlrow ,olleH
 
-# Remove Duplicates using two pointers
+# Remove Duplicates: Given a sorted array, remove the duplicates in-place and return the new length of the array.
 
 arr = [0, 0, 1, 1, 2, 2, 3, 4, 4, 5]
 
@@ -92,7 +92,7 @@ def remove_duplicates(arr):
 print(remove_duplicates(arr))  # [0, 1, 2, 3, 4, 5]
 
 
-# Merge Two Sorted Arrays
+# Merge Two Sorted Arrays: You have two sorted arrays, arr1 and arr2. Merge them into one sorted array.
 
 arr1 = [1, 3, 5, 7]
 arr2 = [2, 4, 6, 8]
@@ -126,4 +126,60 @@ def merge_arrays(array1, array2):
 
 print(merge_arrays(arr1, arr2))
 
+#
+# Palindrome Check: Check if a given string or array is a palindrome using two pointers.
+string = "radar"
+array = [1, 2, 3, 2, 1, 2]
+
+
+def check_palindrome(input_data):
+
+    left_pointer_index = 0
+    right_pointer_index = len(input_data) - 1  # the opposite end of the string
+
+    while left_pointer_index != right_pointer_index or left_pointer_index < right_pointer_index:
+        if input_data[left_pointer_index] != input_data[right_pointer_index]:
+            print("They are not the same therefore not a palindrome")
+            return False
+        else:
+            # increment the indexes towards the centre of the string
+            left_pointer_index += 1
+            right_pointer_index -= 1
+
+    return True  # if the while loop completes without returning False then it is a palindrome
+
+
+print(check_palindrome(array))  # They are not the same therefore not a palindrome - False
+print(check_palindrome(string))  # True
+
+
+# Intersection of Two Arrays: Given two sorted arrays, find their intersection.
+
+arr1 = [1, 2, 2, 3, 4]
+arr2 = [2, 2, 3, 5]
+
+
+def find_intersection(array1, array2):
+
+    pointer1 = 0
+    pointer2 = 0
+
+    intersection = []
+
+    while pointer1 < len(array1) and pointer2 < len(array2):
+        if array1[pointer1] == array2[pointer2]:
+            # intersection found
+            intersection.append(array1[pointer1])
+            pointer1 += 1
+            pointer2 += 1
+        elif array1[pointer1] < array2[pointer2]:
+            # element in array 1 is less than matching element in array 2
+            pointer1 += 1
+        else:
+            pointer2 += 1
+
+    print(intersection)
+
+
+find_intersection(arr1, arr2)
 
